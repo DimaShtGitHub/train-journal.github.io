f159b4a35626426e83b68d4436205ce9777746bd
Shtankevich Dima <schtanckevitch.dima@yandex.ru>
2026-06-01 13:42:08 +0300

    add form for writing train

--- src/pages/NewTrain.jsx
+++ src/pages/NewTrain.jsx
@@ -1,12 +1,12 @@
 import { useState } from "react";
 import { useEffect } from "react";
+import './NewTrain.css'
 
 const NewTrain = () => {
   const [date, setDate] = useState('')
-  
+  const [list, setList] = useState([])
   
   useEffect(() => {
-
     const today = new Date();
 
     const day = today.getDate();
@@ -16,7 +16,6 @@ const NewTrain = () => {
     setDate(`${day}.${month}.${year}`)
   }, [])
   
- 
   const placeTrain = [
     {
       id: 1,
@@ -32,12 +31,67 @@ const NewTrain = () => {
     },
   ];
   
+  const handleSubmit = (event) => {
+    event.preventDefault();
+
+    const gym = event.target.gym.value
+    const num = event.target.num.value
+    const weight = event.target.weight.value
+
+    setList(pre => [...pre, {
+      gym: gym,
+      number: num,
+      weight: weight
+    }])
+
+    event.target.reset();
+  }
+
+  const setTrain = () => {
+    // TODO: отправлять данные тренировки в БД и закрывать форму
+    console.log('Пока не добавил эту функцию :3');
     
+  }
   
-  return <>
-    <h2>{date}: тренировка {placeTrain.find(el => el.id === 1)?.place}</h2>
-    <input></input>
-  </>
+  return <div className="train_box">
+    <div className="train_panel">
+      <form onSubmit={handleSubmit}>
+        <p>
+          {/* <label form="gym">Упражнение:</label> */}
+          <input type="text" name="gym" placeholder="Упражнение:"></input>
+        </p>
+
+        <p>
+          {/* <label form="num">Количество повторений:</label> */}
+          <input type="text" name="num" placeholder="Количество повторений:"></input>
+        </p>
+
+        <p>
+          {/* <label form="weight">Дополнительный вес:</label> */}
+          <input 
+            type="text" 
+            name="weight"
+            placeholder="Дополнительный вес:"
+            ></input>
+        </p>
+        <button type="submit">добавить в тренировку</button>
+      </form>
+    </div>
+
+    <div className="train_list">
+      <h2>{date}: тренировка {placeTrain.find(el => el.id === 1)?.place}</h2>
+      
+      {list.map((el, index) => {
+        return (
+          <div>
+            <p>{index + 1}. {el.gym} / {el.number} / {el.weight}`</p>
+          </div>
+        )
+      })}
+
+      <button onClick={setTrain}>Сохранить тренировку</button>
+    </div>
+  </div>
 }
 
 export default NewTrain
